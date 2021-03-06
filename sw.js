16a405c11988fd2f4deb0e197d59af3280560b07
TijnR <37410925+TijnR@users.noreply.github.com>
2021-03-06 09:33:58 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -32,13 +32,25 @@ self.addEventListener('install', (event) => {
     )
 })
 
-self.addEventListener('fetch', event => {
-    event.respondWith(caches.match(event.request)
-    .then(cachedResponse => {
-        return cachedResponse || fetch(event.request)
-    })
-    .catch((error) => { console.error('Error:', error);
-    })
-    )
-})
-
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          if (response) {
+            return response;     // if valid response is found in cache return it
+          } else {
+            return fetch(event.request)     //fetch from internet
+              .then(function(res) {
+                return caches.open(staticCacheName)
+                  .then(function(cache) {
+                    cache.put(event.request.url, res.clone());    //save the response for future
+                    return res;   // return the fetched data
+                  })
+              })
+              .catch(function(err) {       // fallback mechanism
+                console.log('ERROR: ' + err)
+              });
+          }
+        })
+    );
+  });    
